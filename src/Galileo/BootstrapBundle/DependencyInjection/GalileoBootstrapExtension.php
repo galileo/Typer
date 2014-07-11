8ef3fc1c16c386f0573b94d4b2b2cf1a5fbaf191
galileo <galileox86@gmail.com>
2014-07-11 18:42:25 +0200

    Add bower - big changes in ecosystem

--- src/Galileo/BootstrapBundle/DependencyInjection/GalileoBootstrapExtension.php
+++ src/Galileo/BootstrapBundle/DependencyInjection/GalileoBootstrapExtension.php
@@ -24,5 +24,8 @@ class GalileoBootstrapExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+
+//        $container->setParameter('database.user', 'galileo');
     }
 }
